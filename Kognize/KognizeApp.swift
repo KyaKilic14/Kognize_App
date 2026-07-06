664c65b98cb69b0be748582350a1ecd4b28ee612
Kya Kilic <kyakilic@hotmail.com>
2026-07-06 16:03:14 +0100

    Initial Commit

---
+++ Kognize/KognizeApp.swift
@@ -0,0 +1,32 @@
+//
+//  KognizeApp.swift
+//  Kognize
+//
+//  Created by Kya Kılıç on 06/07/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KognizeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
